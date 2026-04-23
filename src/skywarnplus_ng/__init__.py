23f77fa065371a9716dbf2cb1ce58f097d9c89f4
Jory A. Pratt <geekypenguin@gmail.com>
2026-04-23 17:02:16 -0500

    chore: bump version to 1.0.9

--- src/skywarnplus_ng/__init__.py
+++ src/skywarnplus_ng/__init__.py
@@ -2,6 +2,6 @@
 SkywarnPlus-NG - Modern weather alert system for Asterisk/app_rpt nodes.
 """
 
-__version__ = "1.0.8"
+__version__ = "1.0.9"
 __author__ = "SkywarnPlus-NG Contributors"
 __license__ = "GPL-3.0"
